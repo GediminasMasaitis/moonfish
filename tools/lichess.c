bc6f8f2d6bc0fe8f793d5ef1493d2c5d950cc4cc
zamfofex <zamfofex@twdb.moe>
2023-10-09 22:25:16 -0300

    refuse blitz challenges too (for now)

--- tools/lichess.c
+++ tools/lichess.c
@@ -766,7 +766,7 @@ static void moonfish_handle_events(
 		speed = cJSON_GetObjectItem(challenge, "speed");
 		if (!cJSON_IsString(speed)) moonfish_json_error(argv0);
 		
-		if (!strcmp(speed->valuestring, "bullet"))
+		if (!strcmp(speed->valuestring, "bullet") || !strcmp(speed->valuestring, "blitz"))
 		{
 			snprintf(line, sizeof line, "POST /api/challenge/%s/decline", id->valuestring);
 			if (moonfish_basic_request(argv0, name, port, token, line, "", "reason=tooFast"))
